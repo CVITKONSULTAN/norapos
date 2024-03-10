49460ea2d35c6697b4d7a6dcdf298c23f76c90c3
hai-ru <mheru@student.untan.ac.id>
2024-03-10 20:27:49 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -34,12 +34,12 @@ Route::group(['domain' => '{domain}.{tld}'], function() use($database_domain){
 });
 
 Route::get('/command', function () {
-    Artisan::call('migrate');
-    Artisan::call('db:seed');
+    // Artisan::call('migrate');
+    // Artisan::call('db:seed');
     // Artisan::call('make:controller MultiDomainController');
     // Artisan::call('migrate:rollback');
     // Artisan::call('make:model Blog -m');
-    // Artisan::call('db:seed --class=AddPermissionAbsensiSeeder');
+    Artisan::call('db:seed --class=JaplinSeeder');
     return "OK";
 });
 

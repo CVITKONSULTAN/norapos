d694b5dbda613a4cb86fa37cb6b97fc2cac4bcf7
hai-ru <mheru@student.untan.ac.id>
2024-09-29 14:37:14 +0700

    up

--- resources/views/compro/koneksiedu/layouts/app.blade.php
+++ resources/views/compro/koneksiedu/layouts/app.blade.php
@@ -1,9 +1,12 @@
+@php
+    $title = $domain ?? env('APP_TITLE');
+@endphp
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title',env('APP_TITLE'))</title>
+    <title>@yield('title',$title)</title>
     <meta name="description" content="{{env('APP_TITLE')}}" />
     <link defer href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">

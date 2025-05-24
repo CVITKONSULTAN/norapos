f6ce6ffa40a15f73afc593044aff5743d6ba7d55
hai-ru <mheru@student.untan.ac.id>
2025-05-24 14:54:34 +0700

    fix: restore blog data retrieval logic in list_blog method

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -1147,17 +1147,17 @@ class APIController extends Controller
         // Log::info('$category >> '.$category);
         // Log::info('$skip >> '.$skip);
         
-        // $data = \App\Blog::where('business_id',$business_id)
-        // ->where('category',$category)
-        // ->orderBy('id','DESC')
-        // ->skip($skip)
-        // ->take(10)
-        // ->get();
+        $data = \App\Blog::where('business_id',$business_id)
+        ->where('category',$category)
+        ->orderBy('id','DESC')
+        ->skip($skip)
+        ->take(10)
+        ->get();
 
         // Log::info('$data >> '.json_encode($data));
         // dd(\App\Blog::all(),$data,$business_id,$category,$skip);
 
-        $data = \App\Blog::all();
+        // $data = \App\Blog::all();
 
         return Helper::DataReturn(true,"OK",$data);
     }

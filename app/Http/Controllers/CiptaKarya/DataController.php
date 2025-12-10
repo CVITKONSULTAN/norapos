c886699ea09aa432793153d13ab274c0154a3ce8
hai-ru <mheru@student.untan.ac.id>
2025-12-10 13:29:33 +0700

    up

--- app/Http/Controllers/CiptaKarya/DataController.php
+++ app/Http/Controllers/CiptaKarya/DataController.php
@@ -1224,6 +1224,7 @@ class DataController extends Controller
         $emailAdmin = User::role("Admin#$business_id")->get();
         
         if ($emailAdmin->count() > 0) {
+            Log::info('emailadmin>>', json_encode($emailAdmin));
             $emailList = $emailAdmin->pluck('email')->toArray();
             \Mail::to($emailList)->send(new \App\Mail\DokumenTerbitMail($pengajuan));
         }
